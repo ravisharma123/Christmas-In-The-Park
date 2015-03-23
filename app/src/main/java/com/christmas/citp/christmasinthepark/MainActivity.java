c3a54133483a46f49d40aeb90fb89f39685e0179
Benton Li <bentonli@comcast.net>
2015-03-22 19:10:34 -0700

    added button functions (they don't work yet tho)

--- app/src/main/java/com/christmas/citp/christmasinthepark/MainActivity.java
+++ app/src/main/java/com/christmas/citp/christmasinthepark/MainActivity.java
@@ -1,17 +1,61 @@
 package com.christmas.citp.christmasinthepark;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button mFB;
+    private Button mInsta;
+    private Button mTwitter;
+    private Button mYT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFB = (Button)findViewById(R.id.fbicon);
+        mFB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent to open FB Activity
+                // startActivity();
+            }
+        });
+
+        mTwitter = (Button)findViewById(R.id.twtricon);
+        mTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent to open Twitter Activity
+                // startActivity();
+            }
+        });
+
+        mYT = (Button)findViewById(R.id.yticon);
+        mYT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent to open Youtube Activity
+                // startActivity();
+            }
+        });
+
+        mInsta = (Button)findViewById(R.id.instaicon);
+        mInsta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent to open Instagram Activity
+                // startActivity();
+            }
+        });
     }
 
 
